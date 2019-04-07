0cc57aaa4c4200430b2ca209093131df6d0aafd4
roy yair <roy.yair@gmail.com>
2019-04-07 20:18:03 +0300

    use above20 with reduce + remove unused functions

--- index.js
+++ index.js
@@ -1,57 +1,12 @@
-const ages     = generateAgeArray(1000);
-const pyramid1 = toPyramid(ages);
-const pyramid2 = toPyramidWithReduce1(ages);
-const pyramid3 = toPyramidWithReduce2(ages);
+const ages    = generateAgeArray(10000);
+const pyramid = toPyramid(ages);
 
-console.log(above20(pyramid1));
-console.log(above20(pyramid2));
-console.log(above20(pyramid3));
+console.log(above20(pyramid));
 
 function toPyramid(arr) {
-  let pyramid = {};
-
-  for (let i = 0; i < arr.length; i++) {
-    const { age, gender } = arr[i];
-    const group           = Math.floor(age / 5) * 5;
-
-    // init pyramid's group of age
-    pyramid[group] = pyramid[group] || {};
-
-    //init array by gender in group of age
-    pyramid[group][gender] = pyramid[group][gender] || [];
-
-    //push a person to its place
-    pyramid[group][gender].push(arr[i]);
-  }
-
-  return pyramid;
-}
-
-function toPyramidWithReduce1(arr) {
-  let pyramid = {};
-
-  for (let i = 0; i <= 120; i += 5) {
-    const min = i;
-    const max = i + 5;
-
-    const reducedByGroupOfAges = arr.reduce((acc, cur) => {
-      const { gender, age } = cur;
-
-      (age >= min && age < max) ? (acc[gender] = acc[gender] || []).push(cur) : undefined;
-
-      return acc;
-    }, {});
-
-    pyramid[min] = reducedByGroupOfAges;
-  }
-
-  return pyramid;
-}
-
-function toPyramidWithReduce2(arr) {
   return arr.reduce((acc, cur) => {
     const { gender, age } = cur;
-    const group = Math.floor(age / 5) * 5;
+    const group           = Math.floor(age / 5) * 5;
 
     // init pyramid's group of age
     acc[group] = acc[group] || {};
@@ -67,15 +22,12 @@ function toPyramidWithReduce2(arr) {
 }
 
 function above20(pyramid) {
-  let sum = 0;
-
-  for (let key in pyramid) {
-    if (key > 20) {
-      pyramid[key]['f'] && (sum += pyramid[key]['f'].length);
-    }
-  }
-
-  return sum;
+  return Object.keys(pyramid).reduce((sum, key) => {
+    (key > 20)
+    && pyramid[key]['f']
+    && (sum += pyramid[key]['f'].length);
+    return sum;
+  }, 0);
 }
 
 function generateAgeArray(numOfAges) {
